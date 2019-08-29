08ff9184956acb19aca669b429ffeaab9748b7fc
DoorMamu <mmkkolhapur@gmail.com>
2019-08-29 16:28:04 +0530

    background for main activity

--- app/src/main/java/com/example/sqlite/MainActivity.java
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
@@ -20,7 +21,7 @@ import java.util.Locale;
  */
 public class MainActivity extends AppCompatActivity implements Button.OnClickListener {
 EditText ma_et_name,ma_et_roll,ma_et_marks;
-Button ma_btn_add,ma_btn_del,ma_btn_mod,ma_btn_vi,ma_btn_viall,ma_btn_show;
+Button ma_btn_add,ma_btn_del,ma_btn_mod,ma_btn_vi,ma_btn_viall,ma_btn_show,ma_btn_delall;
 SQLiteDatabase db;
 //***********Step1*************************
 //Prepare  initialization and listener,and create db and table if not exist
@@ -35,6 +36,7 @@ SQLiteDatabase db;
         ma_btn_vi=findViewById(R.id.btn_view);
         ma_btn_viall=findViewById(R.id.btn_view_all);
         ma_btn_show=findViewById(R.id.btn_show);
+        ma_btn_delall=findViewById(R.id.btn_del_all);
 
         ma_btn_add.setOnClickListener(this);
         ma_btn_del.setOnClickListener(this);
@@ -42,6 +44,7 @@ SQLiteDatabase db;
         ma_btn_vi.setOnClickListener(this);
         ma_btn_viall.setOnClickListener(this);
         ma_btn_show.setOnClickListener(this);
+        ma_btn_delall.setOnClickListener(this);
         //*****************Step 2********************
         db=openOrCreateDatabase("StudendDB", Context.MODE_PRIVATE,null);
         db.execSQL("CREATE TABLE IF NOT EXISTS student(rollno VARCHAR,name VARCHAR,marks VARCHAR);");
@@ -69,14 +72,103 @@ SQLiteDatabase db;
         builder.setCancelable(true);
         builder.setTitle(title);
         builder.setMessage(msg);
+        builder.setIcon(R.mipmap.ic_launcher_round);
         builder.show();
     }
     public void insert()
     {
+        //insert the record into the database
         db.execSQL("INSERT INTO student VALUES ('"+ma_et_roll.getText()+"','"+ma_et_name.getText()+"','"+ma_et_marks.getText()+"')");
         showMessage("Success","Record Added");
+        clearEditText();
+    }
+    public void delete()
+    {
+        //get the row(s) retrieved into the cursor
+        Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+ma_et_roll.getText()+"'",null);
+        //if move to the first row op is success then delete else display invalid roll
+        if(c.moveToFirst())
+        {
+            db.execSQL("DELETE FROM student WHERE rollno='"+ma_et_roll.getText()+"'");
+            showMessage("Success","Record Deleted");
+        }
+        else
+        {
+            showMessage("Error","Invalid Roll");
+        }
+        clearEditText();
     }
 
+    public void modify()
+    {
+        Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+ma_et_roll.getText()+"'",null);
+        //if move to the first row op is success then update else display invalid roll
+
+        if(c.moveToFirst())
+        {
+            db.execSQL("UPDATE student SET name='"+ma_et_name.getText()+"',marks='"+ma_et_marks.getText()+"' WHERE rollno='"+ma_et_roll.getText()+"'");
+            showMessage("Success","Record Modified");
+        }
+        else
+        {
+            showMessage("Error","Invalid Roll");
+        }
+        clearEditText();
+    }
+    public void view()
+    {
+        //get the row(s) retrieved into the cursor
+        Cursor c=db.rawQuery("SELECT * FROM student WHERE rollno='"+ma_et_roll.getText()+"'",null);
+        //if move to the first row op is success then Display else display invalid roll
+        if(c.moveToFirst())
+        {
+            ma_et_name.setText(c.getString(1));
+            ma_et_marks.setText(c.getString(2));
+        }
+        else
+        {
+            showMessage("Error","Invalid Roll");
+            clearEditText();
+        }
+
+    }
+    public void viewAll()
+    {
+        //get the row(s) retrieved into the cursor
+        Cursor c=db.rawQuery("SELECT * FROM student",null);
+        //if move to the first row op is success then Display else display invalid roll
+        if(c.getCount()==0)
+        {
+            showMessage("Student details","No records Exists");
+            return;
+        }
+        StringBuffer buffer=new StringBuffer();
+        while (c.moveToNext())
+        {
+            buffer.append("Roll No : "+c.getString(0)+"\n");
+            buffer.append("Name : "+c.getString(1)+"\n");
+            buffer.append("Marks : "+c.getString(2)+"\n\n");
+        }
+        showMessage("Student Details",buffer.toString());
+    }
+
+    public void delAll()
+    {
+        //get the row(s) retrieved into the cursor
+        Cursor c=db.rawQuery("SELECT * FROM student",null);
+        //if move to the first row op is success then Delete all
+        if(c.getCount()==0)
+        {
+            showMessage("Info","Records already deleted");
+            return;
+        }
+        while(c.moveToNext())
+        {
+            db.execSQL("DELETE FROM student WHERE rollno='"+c.getString(0)+"'");
+        }
+        showMessage("Student Details","All records deleted");
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,15 +192,15 @@ SQLiteDatabase db;
                 }
                 //************stp 4*********************
                 insert();
-                clearEditText();
                 //Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_del:
-                if(checkForEmptyInput())
+                if(ma_et_roll.getText().toString().trim().length()==0)
                 {
-                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                    showMessage("Error","Please enter Roll no");
                     return;
                 }
+                delete();
                 //Toast.makeText(this, "Del", Toast.LENGTH_SHORT).show();
 
                 break;
@@ -118,35 +210,40 @@ SQLiteDatabase db;
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                modify();
                 //Toast.makeText(this, "Modify", Toast.LENGTH_SHORT).show();
 
                 break;
             case R.id.btn_view:
-                if(checkForEmptyInput())
+                if(ma_et_roll.getText().toString().trim().length()==0)
                 {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                     return;
                 }
+                view();
                 //Toast.makeText(this, "View", Toast.LENGTH_SHORT).show();
 
                 break;
             case R.id.btn_view_all:
-                if(checkForEmptyInput())
+                /*if(checkForEmptyInput())
                 {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                     return;
-                }
+                }*/
                 //Toast.makeText(this, "View All", Toast.LENGTH_SHORT).show();
-
+                viewAll();
                 break;
             case R.id.btn_show:
-                if(checkForEmptyInput())
+                /*if(checkForEmptyInput())
                 {
                     Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
                     return;
-                }
+                }*/
                 //Toast.makeText(this, "Show", Toast.LENGTH_SHORT).show();
-
+                showMessage("Student Record Application ","Developed By Chandan Prasad Enhanced By Mukund Kulkarni");
+                break;
+            case R.id.btn_del_all:
+                delAll();
                 break;
         }
     }
